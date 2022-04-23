3700085b278ec1cc3410bce0193d9215ab78db37
1CarlosRenan <carlosrenan96@gmail.com>
2022-04-23 11:39:52 -0300

    Adicionando alguns estilos e comportamentos suaves com class ativo

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,8 +1,19 @@
 let nextNumber = document.querySelector('#number');
 const btnAdd = document.querySelector('button');
 let textArea = document.querySelector('#number-list');
+const btnSubmit = document.querySelector('.btnSubmit');
+const listResult = document.querySelectorAll('.result');
 //textArea.value = 2;
 btnAdd.addEventListener('click', addNumberList);
+btnSubmit.addEventListener('click', showResult);
+
+function showResult(e){
+    e.preventDefault();
+
+    listResult.forEach(element => {
+        element.classList.add('ativo')
+    })
+}
 
 function addNumberList() {
     if (!textArea.value) {
